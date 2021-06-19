9e5b04fca34430602fa401db5a1e2cfb67a11c72
Nazan Bahar <77123580+nazanbahar@users.noreply.github.com>
2021-06-19 14:49:48 +0300

    Signed Out.jsx and SingedIn.jsx components has been changed

--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Menu, Image, Dropdown } from "semantic-ui-react"; //intelicense
 
 export default function SignedIn() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <Menu.Item>
+        <Image
+          avatar
+          spaced="right"
+          src="https://avatars.githubusercontent.com/u/77123580?s=400&u=b8eccdb61d901b320fec71878787f37ed0fca9ac&v=4"
+        />
 
+        <Dropdown>
+          <Dropdown.Menu>
+            <Dropdown.Item text="Bilgilerim" icon="info" />
+            <Dropdown.Item text="Çıkış" icon="sign-out" />
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Item>
+    </div>
+  );
+}
--- src/layouts/SignedOut.jsx
+++ src/layouts/SignedOut.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Button } from "semantic-ui-react"; //intelisence
+
 
 export default function SignedOut() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Button>Giriş yap</Button>
+      <Button>Kayıt ol</Button>
+    </div>
+  );
 }
-
-
-/*
-Steps - SignedOut.jsx NOTE
-step1: rfc
-
-*/
