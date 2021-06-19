51617499995af99e65dcc211d2f5ab5a45f68b29
Nazan Bahar <77123580+nazanbahar@users.noreply.github.com>
2021-06-19 14:46:49 +0300

    Signed Out.jsx and SingedIn.jsx components has been added with rfc.

---
+++ src/layouts/SignedOut.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SignedOut() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+/*
+Steps - SignedOut.jsx NOTE
+step1: rfc
+
+*/
